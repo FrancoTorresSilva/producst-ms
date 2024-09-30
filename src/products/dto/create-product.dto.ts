6e7d2e884d052802240675cf60fc798f8fbb598d
Franco Torres Silva <franco.t.silva99@gmail.com>
2024-09-30 11:45:25 +0200

    añadidas variables de entorno

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,15 @@
-export class CreateProductDto {}
+import { Type } from "class-transformer";
+import { IsNotEmpty, IsNumber, IsString, Min } from "class-validator";
+
+export class CreateProductDto {
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+    @IsNumber({
+        maxDecimalPlaces: 4
+    })
+    @Min(0)
+    @Type(() => Number)
+    price:string;
+
+}
