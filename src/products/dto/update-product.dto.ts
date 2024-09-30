c79a2121e743e2a794c4d86f40c13ab1c550dab4
Franco Torres Silva <franco.t.silva99@gmail.com>
2024-09-30 14:10:54 +0200

    cambiando a mircroservicios

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,8 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
-import { IsNumber, IsString, Min } from 'class-validator';
+import { IsNumber, IsPositive, IsString, Min } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class UpdateProductDto extends PartialType(CreateProductDto) {
-   
+   @IsNumber()
+   @IsPositive()
+   id: number;
 }
